c3ed775fb619abb336c85f436d77ceae21994972
Koji NAKAMURA <kozy4324@gmail.com>
2025-05-19 22:58:55 +0900

    Update euler001.rb

--- euler001.rb
+++ euler001.rb
@@ -1,13 +1,15 @@
-def numbers(up_to)
-  (1...up_to).to_a
-end
+class Object
+  def numbers(up_to)
+    (1...up_to).to_a
+  end
 
-def only_multiples_of_3_or_5(numbers)
-  numbers.map { |n| n % 3 == 0 || n % 5 == 0 ? n : 0 }
-end
+  def only_multiples_of_3_or_5(numbers)
+    numbers.map { |n| n % 3 == 0 || n % 5 == 0 ? n : 0 }
+  end
 
-def sum_up(numbers)
-  numbers.reduce(0) { |sum, it| sum + it }
+  def sum_up(numbers)
+    numbers.reduce(0) { |sum, it| sum + it }
+  end
 end
 
 puts sum_up only_multiples_of_3_or_5 numbers 1000
